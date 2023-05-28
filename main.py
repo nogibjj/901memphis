d7d9ce5e402617443d9caba1cc42322c0fef1946
abarnett1999 <87715968+abarnett1999@users.noreply.github.com>
2023-05-28 03:44:23 +0000

    huggigface-webapp-api

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-"""
-Main cli or app entry point
-"""
+# """
+# Main cli or app entry point
+# """
 
-from mylib.calculator import add
-import click
-
-
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+# from mylib.calculator import add
+# import click
 
 
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+# @click.command("add")
+# @click.argument("a", type=int)
+# @click.argument("b", type=int)
+# def add_cli(a, b):
+#     click.echo(add(a, b))
+
+
+# if __name__ == "__main__":
+#     # pylint: disable=no-value-for-parameter
+#     add_cli()
+from transformers import pipeline
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+
+generator = pipeline('text-generation', model='gpt2')
+
+app = FastAPI()
+
+
+class Body(BaseModel):
+    text: str
+
+
+@app.get('/')
+def root():
+    return HTMLResponse("<h1>A self-documenting API to interact with a GPT2 model and generate text</h1>")
+
+
+@app.post('/generate')
+def predict(body: Body):
+    results = generator(body.text, max_length=35, num_return_sequences=1)
+    return results[0]
